82c76c57511fa7b10ec551373727f7b8653c4377
kaxla <kaylas.email.address@gmail.com>
2014-02-20 16:01:16 -0800

    chapter 2

--- lib/rulers/routing.rb
+++ lib/rulers/routing.rb
@@ -1,7 +1,8 @@
 module Rulers
   class Application
     def get_controller_and_action(env)
-      _, cont, action, after = env["PATH_INFO"].split('/', 4)
+      _, cont, action, after =
+        env["PATH_INFO"].split('/', 4)
       cont = cont.capitalize #"People"
       cont += "Controller" #"PeopleController"
 
